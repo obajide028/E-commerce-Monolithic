778d4b996ae7f642f287609624e0937959c75406
obajide028 <obajide028@gmail.com>
2026-02-08 23:26:15 +0000

    product entity

---
+++ e-com-mo/src/main/java/com/example/e_com_mo/model/Product.java
@@ -0,0 +1,11 @@
+package com.example.e_com_mo.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Product {
+}
